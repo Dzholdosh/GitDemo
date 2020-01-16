50669eb948d2f2d91053fb4a2cec0db6fc74758e
Dzholdosh <47745478+Dzholdosh@users.noreply.github.com>
2020-01-16 12:29:34 +0600

    Update Appdelegate.swift

--- GitDemo/AppDelegate.swift
+++ GitDemo/AppDelegate.swift
@@ -17,7 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         print("Hello, World!")
-        
+        print("Another HELLO, World!")
+
         return true
     }
 
